84e4334bb80e68e53e0813e9618f34f35d79c11e
Oleg Dramaretskyi <dramarec@gmail.com>
2021-05-19 07:21:32 +0300

    added screen two ProjectItem

---
+++ client/components/ProjectItem/index.tsx
@@ -0,0 +1,41 @@
+import React from 'react';
+import { View, Text, Pressable } from 'react-native';
+import { MaterialCommunityIcons } from '@expo/vector-icons';
+import styles from './styles';
+
+interface ProjectItemProps {
+    project: {
+        id: string;
+        title: string;
+        createdAt: string;
+    };
+}
+
+const ProjectItem = ({ project }: ProjectItemProps) => {
+    const onPress = () => {
+        console.warn(`open ${project.title}`);
+        // navigation.navigate('ToDoScreen', { id: project.id })
+    };
+    return (
+        <Pressable onPress={onPress} style={styles.rootContainer}>
+            <View style={styles.iconContainer}>
+                <MaterialCommunityIcons
+                    name="file-outline"
+                    size={24}
+                    color="white"
+                />
+            </View>
+            <View
+                style={{
+                    flexDirection: 'row',
+                    alignItems: 'center',
+                }}
+            >
+                <Text style={styles.title}>{project.title}</Text>
+                <Text style={styles.time}>{project.createdAt}</Text>
+            </View>
+        </Pressable>
+    );
+};
+
+export default ProjectItem;
--- client/screens/TabTwoScreen.tsx
+++ client/screens/TabTwoScreen.tsx
@@ -1,32 +1,48 @@
-import * as React from 'react';
-import { StyleSheet } from 'react-native';
+import React, { useState } from 'react';
+import { StyleSheet, FlatList } from 'react-native';
 
-import EditScreenInfo from '../components/EditScreenInfo';
+// import EditScreenInfo from '../components/EditScreenInfo';
 import { Text, View } from '../components/Themed';
+import ProjectItem from '../components/ProjectItem';
+
+const initialState = [
+    {
+        id: '1',
+        title: 'Project 1',
+        createdAt: '2d',
+    },
+    {
+        id: '2',
+        title: 'Project 2',
+        createdAt: '2d',
+    },
+    {
+        id: '3',
+        title: 'Project 3',
+        createdAt: '2d',
+    },
+];
 
 export default function TabTwoScreen() {
-  return (
-    <View style={styles.container}>
-      <Text style={styles.title}>Tab Two</Text>
-      <View style={styles.separator} lightColor="#eee" darkColor="rgba(255,255,255,0.1)" />
-      <EditScreenInfo path="/screens/TabTwoScreen.tsx" />
-    </View>
-  );
+    const [project, setProject] = useState(initialState);
+
+    return (
+        <View style={styles.container}>
+            <FlatList
+                data={project}
+                style={{ width: '100%' }}
+                renderItem={({ item }) => (
+                    <ProjectItem project={item} />
+                )}
+            />
+        </View>
+    );
 }
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-  title: {
-    fontSize: 20,
-    fontWeight: 'bold',
-  },
-  separator: {
-    marginVertical: 30,
-    height: 1,
-    width: '80%',
-  },
+    container: {
+        flex: 1,
+        alignItems: 'center',
+        justifyContent: 'center',
+    },
 });
